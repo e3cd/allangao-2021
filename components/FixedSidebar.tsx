8b74c45ad450663fee0966665e869e753d7e0d24
Allan Gao <allangao95@gmail.com>
2021-03-21 21:44:43 +1100

    adjust margins on fixed sidebar

--- components/FixedSidebar.tsx
+++ components/FixedSidebar.tsx
@@ -12,11 +12,11 @@ export default function FixedSidebar({ homeAttributes }: Props) {
 
   return (
     <div className="w-full lg:w-2/5">
-      <div className="flex flex-col w-full pr-8 py-16 mx-auto pl-8 lg:pl-16 mt-4 lg:mt-32">
-        <h1 className="mt-12 mb-8 text-base font-bold text-center  tracking-relaxed md:text-left title-font text-lightBlack dark:text-lightGrey lg:text-left lg:text-7xl title-font">
+      <div className="flex flex-col w-full pr-8 py-8 mx-auto pl-8 lg:pl-16 mt-4 lg:mt-32">
+        <h1 className="mt-2  mb-8 text-base font-bold text-center  tracking-relaxed md:text-left title-font text-lightBlack dark:text-lightGrey lg:text-left lg:text-7xl title-font">
           {homeAttributes?.hero_title}
         </h1>
-        <p className=" text-base leading-snug text-left tracking-relaxed lg:text-2xl lg:pr-10 text-paragraphLight dark:text-mediumGrey lg:text-left lg:text-1xl">
+        <p className=" text-base leading-snug text-left tracking-relaxed sm:text-xl lg:pr-10 text-paragraphLight dark:text-mediumGrey lg:text-left lg:text-1xl">
           {homeAttributes?.hero_description}
         </p>
       </div>
